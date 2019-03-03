551b0c99b2ba4fa012ff22f0a8fd77847d5dfb39
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-03-03 20:10:40 +0200

    Course done

--- src/App.js
+++ src/App.js
@@ -1,57 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
-import uuid from 'uuid';
+// import uuid from 'uuid';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
     state = {
-        todos: [
-            {
-                id: uuid.v4(),
-                title: 'Take out the trash',
-                completed: false
-            },
-            {
-                id: uuid.v4(),
-                title: 'Dinner with Krissu',
-                completed: true
-            },
-            {
-                id: uuid.v4(),
-                title: 'Wash the dishes',
-                completed: false
-            }
-        ]
+        todos: []
     }
 
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            .then(res => this.setState({ todos: res.data }))
+    }
+
+    // Toggle Complete
     markComplete = (id) => {
-        this.setState({
-            todos: this.state.todos.map(todo => {
-                if (todo.id === id) {
+        this.setState({ todos: this.state.todos.map(todo => {
+                if(todo.id === id) {
                     todo.completed = !todo.completed
                 }
                 return todo;
-            })
-        });
+            }) });
     }
 
+    // Delete Todo
     delTodo = (id) => {
-        this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id)]});
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+            .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
     }
 
-    //addtodo
+    // Add Todo
     addTodo = (title) => {
-        const newTodo = {
-            id: uuid.v4(),
+        axios.post('https://jsonplaceholder.typicode.com/todos', {
             title,
             completed: false
-        }
-        this.setState({todos: [...this.state.todos, newTodo]})
+        })
+            .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
     }
 
     render() {
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react'
 
 function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>This is the TodoList app v1.0 It is  part of a React crash course</p>
+            <p>This is the TodoList app v1.0.0. It is part of a React crash course</p>
         </React.Fragment>
     )
 }
+
 
 export default About;

b9f50d108b47a97d19a6d9617346589918776fca
fredvunkiktkhk <45164767+fredvunkiktkhk@users.noreply.github.com>
2019-03-03 16:41:40 +0200

    Toggle complete ( checkboxes)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Todos from './components/Todos';
 
 import './App.css';
 
 class App extends Component {
-  state = {
-    todos: [
-      {
-        id: 1,
-        title: 'Take out the trash',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Dinner with Krissu',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Wash the dishes',
-        completed: false
-      }
-    ]
-  }
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: 'Take out the trash',
+                completed: false
+            },
+            {
+                id: 2,
+                title: 'Dinner with Krissu',
+                completed: true
+            },
+            {
+                id: 3,
+                title: 'Wash the dishes',
+                completed: false
+            }
+        ]
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <Todos todos={this.state.todos}/>
-      </div>
-    );
-  }
+    markComplete = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo;
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+            </div>
+        );
+    }
 }
 
 export default App;
